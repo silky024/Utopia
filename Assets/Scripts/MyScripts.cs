b655a8df33b9d35b96cf696f7b71be6f6bfb9eec
suphawan <suphawan.phunkhamtan@gmail.com>
2021-09-02 12:51:06 +0700

    Homework1

---
+++ Assets/Scripts/MyScripts.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScripts : MonoBehaviour
+{
+    int width, height, result, i, a;
+    float f;
+    char c;
+    bool b;
+    double d;
+
+    // Start is called before the first frame update
+     void Start()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            print(i);
+        }
+        
+        for (int i = 0; i <= 100; i++)
+        {
+            if ((i % 2) == 0)
+            {
+                print(i);
+            }
+            // if inside ( 9 % 2 => if reminder 0 even, if reminder 1 odd )
+        float a = 4.0f;
+        float r = circle(a);
+        print(r);
+        
+        }
+        float circle(float a)
+        {
+            float result;
+            a = 4.0f;
+            result = 3.14f * a;
+
+            print(result);    
+            return (result);
+        }
+
+    }   
+}
